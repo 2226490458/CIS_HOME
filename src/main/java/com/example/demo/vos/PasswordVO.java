0017918170cfa6d4586b87faf31659bd78ad6cf6
Baiyusoup <1575727840@qq.com>
2021-06-19 15:17:44 +0800

    '添加修改密码功能'

--- src/main/java/com/example/demo/vos/PasswordVO.java
+++ src/main/java/com/example/demo/vos/PasswordVO.java
@@ -4,5 +4,42 @@ package com.example.demo.vos;
  * @author 青菜白玉堂
  * @create 2021-06-17
  */
+
 public class PasswordVO {
+    private Integer userId;
+    private String oldPsw;
+    private String newPsw;
+
+    @Override
+    public String toString() {
+        return "PasswordVO{" +
+                "userId=" + userId +
+                ", oldPsw='" + oldPsw + '\'' +
+                ", newPsw='" + newPsw + '\'' +
+                '}';
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getOldPsw() {
+        return oldPsw;
+    }
+
+    public void setOldPsw(String oldPsw) {
+        this.oldPsw = oldPsw;
+    }
+
+    public String getNewPsw() {
+        return newPsw;
+    }
+
+    public void setNewPsw(String newPsw) {
+        this.newPsw = newPsw;
+    }
 }
